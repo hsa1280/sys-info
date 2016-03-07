b56d469f41bbb2ac14a524a74cc04251a3723587
Shian Huang <shian.huang@manheim.com>
2016-03-07 14:47:59 -0500

    Changed chart to line

--- scripts/render.js
+++ scripts/render.js
@@ -3,26 +3,32 @@ var data = {
     datasets: [
         {
             label: "1 minute",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
             data: []
         },
         {
             label: "5 minute",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
             data: []
         },
         {
             label: "15 minute",
-            fillColor: "rgba(151,167,195,0.5)",
-            strokeColor: "rgba(151,167,195,0.8)",
-            highlightFill: "rgba(151,167,195,0.75)",
-            highlightStroke: "rgba(151,167,195,1)",
+            fillColor: "rgba(151,167,215,0.2)",
+            strokeColor: "rgba(151,167,215,1)",
+            pointColor: "rgba(151,167,215,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
             data: []
         }
     ]
@@ -36,40 +42,53 @@ $( document ).ready(function() {
    var ctx = $("#myChart").get(0).getContext("2d");
 
    var options = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero : true,
 
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines : true,
+                     ///Boolean - Whether grid lines are shown across the chart
+                     scaleShowGridLines : true,
 
-        //String - Colour of the grid lines
-        scaleGridLineColor : "rgba(0,0,0,.05)",
+                     //String - Colour of the grid lines
+                     scaleGridLineColor : "rgba(0,0,0,.05)",
 
-        //Number - Width of the grid lines
-        scaleGridLineWidth : 1,
+                     //Number - Width of the grid lines
+                     scaleGridLineWidth : 1,
 
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
+                     //Boolean - Whether to show horizontal lines (except X axis)
+                     scaleShowHorizontalLines: true,
 
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
+                     //Boolean - Whether to show vertical lines (except Y axis)
+                     scaleShowVerticalLines: true,
 
-        //Boolean - If there is a stroke on each bar
-        barShowStroke : true,
+                     //Boolean - Whether the line is curved between points
+                     bezierCurve : true,
 
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth : 2,
+                     //Number - Tension of the bezier curve between points
+                     bezierCurveTension : 0.4,
 
-        //Number - Spacing between each of the X value sets
-        barValueSpacing : 5,
+                     //Boolean - Whether to show a dot for each point
+                     pointDot : true,
 
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing : 1,
+                     //Number - Radius of each point dot in pixels
+                     pointDotRadius : 4,
 
-        //String - A legend template
-        legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+                     //Number - Pixel width of point dot stroke
+                     pointDotStrokeWidth : 1,
 
-    };
+                     //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                     pointHitDetectionRadius : 20,
+
+                     //Boolean - Whether to show a stroke for datasets
+                     datasetStroke : true,
+
+                     //Number - Pixel width of dataset stroke
+                     datasetStrokeWidth : 2,
+
+                     //Boolean - Whether to fill the dataset with a colour
+                     datasetFill : true,
+
+                     //String - A legend template
+                     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+                 };
 
    var myLineChart = null;
 
@@ -82,9 +101,9 @@ $( document ).ready(function() {
               data.datasets[ 2 ].data = [];
 
               dataProcessor.queue.forEach(function(loadData, index) {
-                    myLineChart.datasets[ 0 ].bars[ index ].value = loadData.last1Min;
-                    myLineChart.datasets[ 1 ].bars[ index ].value = loadData.last5Min;
-                    myLineChart.datasets[ 2 ].bars[ index ].value = loadData.last15Min;
+                    myLineChart.datasets[ 0 ].points[ index ].value = loadData.last1Min;
+                    myLineChart.datasets[ 1 ].points[ index ].value = loadData.last5Min;
+                    myLineChart.datasets[ 2 ].points[ index ].value = loadData.last15Min;
               });
               last1MinDataSet = myLineChart.datasets[0];
 
@@ -105,7 +124,7 @@ $( document ).ready(function() {
             data.datasets[ 1 ].data.push(0);
             data.datasets[ 2 ].data.push(0);
        }
-       myLineChart = new Chart(ctx).Bar(data, options);
+       myLineChart = new Chart(ctx).Line(data, options);
        updateData(config.frequency);
    });
 
